94e548d143ceab84901285b3bbd9f81d5c84bc09
az-m <alisonzmills@gmail.com>
2025-07-08 09:18:17 +0100

    fix: updated reblog function

--- src/components/ReblogPost.jsx
+++ src/components/ReblogPost.jsx
@@ -14,21 +14,12 @@ export default function ReblogPost({ id, userID }) {
       )
     ).rows[0];
 
-    let temp = parseInt(Math.random() * (1000000 - 1) + 1);
-
-    await db.query(
-      `INSERT INTO posts9 (title, content, user_id, from_user, temp) VALUES ($1, $2, $3, $4, $5)`,
-      [og_post.title, og_post.content, userID, og_post.user_id, temp]
-    );
-
     const new_postID = (
-      await db.query(`SELECT id FROM posts9 WHERE temp = $1`, [temp])
+      await db.query(
+        `INSERT INTO posts9 (title, content, user_id, from_user) VALUES ($1, $2, $3, $4) RETURNING id`,
+        [og_post.title, og_post.content, userID, og_post.user_id]
+      )
     ).rows[0].id;
-
-    await db.query(`INSERT INTO tags9 (post_id) VALUES ($1)`, [new_postID]);
-
-    temp = null;
-    await db.query(`UPDATE posts9 SET temp = $1`, [temp]);
 
     revalidatePath("/");
     revalidatePath("/user");
